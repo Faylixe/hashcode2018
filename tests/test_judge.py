fac8339122e34548acd75431c5dbac73b4a6666a
Felix Voituret <felix.voituret@gmail.com>
2018-02-19 16:37:30 +0100

    Add unit test for judge module

---
+++ tests/test_judge.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+# coding: utf8
+
+""" """
+
+
+def test_build_filelist():
+    """ """
+    pass
+
+
+def test_build_archive_filelist():
+    """ """
+    pass
+
+
+def test_create_source_archive():
+    """ """
+    pass
+
