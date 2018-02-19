3ace199635c8e34853b6265b4d0e853c290e1981
Felix Voituret <felix.voituret@gmail.com>
2018-02-19 14:18:00 +0100

    Add documentation to template

--- utils/template.py
+++ utils/template.py
@@ -1,7 +1,26 @@
 #!/usr/bin/env python
 # coding: utf8
 
-""" TODO : Document. """
+"""
+WARNING
+~~~~~~~
+
+Use DatasetReader class for reading standard input which ensure fastest
+input reading. And use sys.stdout.write() instead of print() for fastest
+standard output writing.
+
+@see https://algocoding.wordpress.com/2015/04/23/fast-io-methods-for-competitive-programming/
+
+EXEMPLE
+~~~~~~~
+
+reader = DatasetReader()        
+a, b, c = reader.next_ints()    # 3 ints on the same line.
+n = reader.next_int()           # 1 int on the line only.
+s = reader.next_line()          # Read the next line as a string.
+r = reader.next_row()           # Read the next line as a map row
+stdout.write('BLABLA')          # FASTER THAN PRINT !
+"""
 
 from sys import stdout
 
@@ -9,20 +28,8 @@ from utils.dataset import DatasetReader
 
 
 def main():
-    """ Place your algorithm here. """
     reader = DatasetReader()
-    # 3 ints on the same line.
-    a, b, c = reader.next_ints()
-    # 1 int on the line only.
-    n = reader.next_int()
-    # Read the next line as a string.
-    s = reader.next_line()
-    # Read the next line as a map row
-    # returned as a list of char, not string
-    r = reader.next_row()
-    r[0] = 'X'
-    # FASTER THAN PRINT !
-    stdout.write('BLABLA')
+    # TODO : Read input, solve problem, write to stdout.
 
 
 if __name__ == '__main__':
