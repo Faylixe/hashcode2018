73f03ceaca86b95832ff6a7017a7f62886f73fb0
Vincent Bouvier <vincent.bouvier@ismart.fr>
2018-03-01 19:18:14 +0100

    Add ride utils methods

--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,11 @@
 def distance(ab, xy):
     return abs(ab[0] - xy[0]) - abs(ab[1] - xy[1])
+
+def ride_start_pos(ride):
+    return (ride[0], ride[1])
+
+def ride_end_pos(ride):
+    return (ride[2], ride[3])
+
+def ride_step_range(ride):
+    return (ride[4], ride[5])
