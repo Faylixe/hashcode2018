d4821a356f01f0e5f3cdb2b97b8b1b4570e3ae75
Vincent Bouvier <vincent.bouvier@ismart.fr>
2018-03-01 20:35:47 +0100

    [RUN] workspace/vb/vibou_2 a

---
+++ submition/dataset.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+# coding: utf8
+
+""" Utility functions for reading standard input. """
+
+from sys import stdin
+
+__author__ = 'fv'
+
+
+class DatasetReader(object):
+    """ Utility class for fast input reading.
+    
+    WARNING
+    ~~~~~~~
+
+    Use DatasetReader class for reading standard input which ensure fastest
+    input reading. And use sys.stdout.write() instead of print() for fastest
+    standard output writing.
+
+    @see https://algocoding.wordpress.com/2015/04/23/fast-io-methods-for-competitive-programming/
+
+    EXEMPLE
+    ~~~~~~~
+
+    reader = DatasetReader()        
+    a, b, c = reader.next_ints()    # 3 ints on the same line.
+    n = reader.next_int()           # 1 int on the line only.
+    s = reader.next_line()          # Read the next line as a string.
+    r = reader.next_row()           # Read the next line as a map row
+    stdout.write('BLABLA')          # FASTER THAN PRINT !
+    """
+
+    def __init__(self, stream=stdin):
+        """ Default constructor. Read the whole standard input. """
+        self._lines = [line.rstrip() for line in stream]
+
+    def next_int(self):
+        """ Returns the next line as a single int. """
+        return self.next_ints()[0]
+
+    def next_ints(self):
+        """ Returns the next line as integer list. """
+        if len(self._lines) == 0:
+            raise ValueError()
+        return [int(x) for x in self._lines.pop(0).split()]
+
+    def next_line(self):
+        """ Returns the next line as a string. """
+        if len(self._lines) == 0:
+            raise ValueError()
+        return self._lines.pop(0)
+
+    def next_row(self):
+        """ Returns the next line as a map row. """
+        if len(self._lines) == 0:
+            raise ValueError()
+        return [c for c in self._lines.pop(0)]
+
+
+def load_dataset_from_file(path):
+    """ Loads the dataset from the given file.
+
+    :returns: Dataset file path.
+    """
+    with open(path, 'r') as stream:
+        return load_dataset(stream)
+
+
+def load_dataset(stream=stdin):
+    """ Loads the dataset from the standard input.
+
+    :returns: Dataset instance in a generic format.
+    """
+    reader = DatasetReader(stream)
+    r, c, f, n, b, t = reader.next_ints()
+    rides = []
+    for i in range(n):
+        a, b, x, y, s, u = reader.next_ints()
+        rides.append((a, b, x, y, s, u))
+    return r, c, f, n, b, t, rides
---
+++ workspace/vb/vibou_2.py
@@ -0,0 +1,149 @@
+#!/usr/bin/env python
+# coding: utf8
+
+""" """
+
+from sys import stdout, stderr
+
+from utils.dataset import load_dataset
+from utils.utils import distance, ride_start_pos, ride_end_pos, ride_step_range
+
+def costForVehicule(vehicule, ride):
+    startPos = ride_start_pos(ride)
+    endPos = ride_end_pos(ride)
+
+    distToStart = distance(startPos, vehicule.position)
+    rideDistance = distance(startPos, endPos)
+    totalDistance = distToStart + rideDistance
+
+    return totalDistance
+
+class Vehicule:
+    def __init__(self, id):
+        self.posx = 0
+        self.posy = 0
+
+        self.nextPosX = 0
+        self.nextPosY = 0
+        self.busyUntil = 0
+        self.isBusy = False
+        self.id = id
+        self.rides = []
+
+    def updateStep(self, step):
+        if step >= self.busyUntil:
+            self.posx = self.nextPosX
+            self.posy = self.nextPosY
+            self.isBusy = False
+
+    @property
+    def position(self):
+        if(self.isBusy):
+            return (self.nextPosX, self.nextPosY)
+
+        return (self.posx, self.posy)
+
+    def canPerformRide(self, ride):
+        if self.isBusy:
+            return False
+
+        return True
+
+        # startPos = ride_start_pos(ride)
+        # endPos = ride_end_pos(ride)
+        # range = ride_step_range(ride)
+
+        # realStart = max(range[0], self.busyUntil)
+        # rideDistance = distance(startPos, endPos)
+
+        # time = realStart - rideDistance
+        # return time >= 0
+
+
+
+    def performRide(self, ride):
+        startPos = ride_start_pos(ride)
+        endPos = ride_end_pos(ride)
+
+        self.nextPosX = endPos[0]
+        self.nextPosY = endPos[1]
+
+        distToStart = distance(startPos, self.position)
+        rideDistance = distance(startPos, endPos)
+        totalDistance = distToStart + rideDistance
+        self.busyUntil += totalDistance
+        self.rides.append(ride[6])
+        self.isBusy = True
+
+
+class VehiculePool:
+
+    def __init__(self, numVehicules):
+        self.vehicules = []
+        for x in range(0, numVehicules):
+            self.vehicules.append(Vehicule(x))
+
+    def closest(self, ride):
+        closest = None
+
+        bestVehicule = None
+        bestCost = None
+        for vehicule in self.vehicules:
+            if vehicule.canPerformRide(ride):
+                cost = costForVehicule(vehicule, ride)
+                log("vehicule %s cost %s" %(vehicule.id, cost))
+                if bestCost is None or cost < bestCost:
+                    bestVehicule = vehicule
+
+        if bestVehicule is not None:
+            log("select vehicule %s" % (bestVehicule.id))
+            return bestVehicule
+
+
+    def tick(self, step):
+        log('======= STEP %s ========' %(step))
+        for vehicule in self.vehicules:
+            vehicule.updateStep(step)
+
+
+
+
+
+def log(message):
+    """ Debug logging """
+    stderr.write('%s\n' % message)
+
+
+def main():
+    r, c, f, n, b, t, rides = load_dataset()
+
+    pool = VehiculePool(f)
+    normalized = []
+    idx = 0
+    for ride in rides:
+        normalized.append([ride[0], ride[1], ride[2], ride[3], ride[4], ride[5], idx])
+        idx += 1
+    normalized.sort(key=lambda ride:ride[4])
+
+    step = -1
+    for ride in normalized:
+        if(ride[4] != step):
+            step = ride[4]
+            pool.tick(step)
+
+        log('--- Ride ---')
+        vehicule = None
+        while(vehicule is None):
+            vehicule = pool.closest(ride)
+            if vehicule is None:
+                step += 1
+                pool.tick(step)
+
+        if vehicule is not None:
+            vehicule.performRide(ride)
+
+    for vehicule in pool.vehicules:
+        print('%s %s' % (str(len(vehicule.rides)), ' '.join([str(r) for r in vehicule.rides])))
+
+if __name__ == '__main__':
+    main()
